acfb5bae4057011d255ac81c7c83f90129d30e39
suzAnnaKom <kompaktorzsu@gmail.com>
2017-08-06 02:31:00 +0200

    Init

--- src/app/js/webshop.js
+++ src/app/js/webshop.js
@@ -2,14 +2,5 @@
 
   'use strict';
 
-  // Full height
-  $(window).on('load resize', function(){
-    setListItemHeight();
-  });
-
-  function setListItemHeight() {
-    var $item = $('.list-categoories li a');
-    //$item.height($item.width());
-  }
 
 })(jQuery);
