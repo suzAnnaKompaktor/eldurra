c8222ea15879a40422f0b9651c972161e1d1d345
suzAnnaKom <kompaktorzsu@gmail.com>
2017-08-07 15:14:27 +0200

    Add Profile

--- dist/js/catalogue.js
+++ dist/js/catalogue.js
@@ -19,5 +19,11 @@
     $('#tabSearchResults').addClass('active').addClass('in');
   });
 
+   $(['href="#tabProfile"']).click(function(e){
+    e.preventDefault();
+    $('#sectionCatalogue .tab-pane.active').removeClass('active').removeClass('in');
+    $('#tabProfile').addClass('active').addClass('in');
+  });
+
 
 })(jQuery);
